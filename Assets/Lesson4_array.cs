88d8f4dae48073e63a0b1f101e366d1ea7b5a7bb
Sato8 <yagami_koushiro@ezweb.ne.jp>
2020-02-17 20:04:36 +0900

    ScriptTestプロジェクト　初めてのコミット
    Lesson4の課題提出用のファイルです。

---
+++ Assets/Lesson4_array.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lesson4_array : MonoBehaviour {
+
+    void Start()
+    {
+        //要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化。
+        int[] array = { 1, 20, 300, 4000, 50000 };
+
+        Debug.Log("順番に表示");
+
+        //配列の要素数を順番に表示。
+        for (int i = 0; i < array.Length; i++){
+            Debug.Log(array[i]);
+        }
+
+        Debug.Log("逆順に表示");
+
+        //配列の各要素を逆順に表示。
+        for (int i = array.Length - 1; i >= 0; i--){
+            Debug.Log(array[i]);
+        }
+    }
+}
